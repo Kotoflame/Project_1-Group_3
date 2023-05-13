2acf32c98aecfc9c9f63513392ab78b925a3f25a
Kotoflame <ryandeancornelius@outlook.com>
2023-05-12 21:40:06 -0500

    updated Ryan Code
    Added new files, added lines for BLS importing, etc,

--- Code/api_keys.py
+++ Code/api_keys.py
@@ -6,3 +6,6 @@ Openweather_api_key = "4fc6c621ec8d496d6762ac6f077f54d8"
 
 # Geoapify API Key
 Geoapify_key = "5f3e3b9c92f0408cae7e05687fdf496c"
+
+#Bureau of Labor Stats Key
+BLS_key = "89c3dcdca6574c02bc9f898e4a2ceb67"
